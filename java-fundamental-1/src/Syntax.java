bce7b88307c0e46f7c9cefa78e794068d67c2857
memory125 <mengde1120@sina.com>
2021-01-14 15:35:12 +0800

    initial Syntax and User module

---
+++ java-fundamental-1/src/Syntax.java
@@ -0,0 +1,25 @@
+import com.wing.module.User;
+import java.util.Random;
+
+public class Syntax {
+    public static void main(String[] args) {
+        User user = new User();
+        user.setName("H1");
+        user.setId(new Random().nextLong());
+        user.setAddr("Shenzhen,Guangdong,China");
+        user.setAge(23);
+        user.setGender("Male");
+        user.setSalary(9000L);
+
+        System.out.println("User Information: ");
+        System.out.println(user.getId() + "\t" +
+                user.getName() + "\t" +
+                user.getGender() + "\t" +
+                user.getAge() + "\t" +
+                user.getAddr() + "\t" +
+                user.getSalary());
+
+
+        System.out.println("The Syntax Demo!");
+    }
+}
---
+++ java-fundamental-1/src/com/wing/module/User.java
@@ -0,0 +1,67 @@
+package com.wing.module;
+
+import java.util.Random;
+import java.util.UUID;
+
+/**
+ * @author memory125
+ */
+public class User {
+    // User Information
+    private long id;
+    private String name;
+    private String gender;
+    private String addr;
+    private int age;
+    private long salary;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(long salary) {
+        this.salary = salary;
+    }
+
+
+}
