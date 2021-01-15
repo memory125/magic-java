4268a6a1ac86dd7707549406860e00f5a7324f00
memory125 <mengde1120@sina.com>
2021-01-15 17:05:46 +0800

    update ScannerDemo4.java

--- java-fundamental-2/src/com/wing/fundamental/ScannerDemo4.java
+++ java-fundamental-2/src/com/wing/fundamental/ScannerDemo4.java
@@ -12,10 +12,12 @@ public class ScannerDemo4 {
 
         System.out.println("请输入数据： ");
 
+        // double类型数据
         while (scanner.hasNextDouble()) {
             x = scanner.nextDouble();
             count++;
             sum = sum + x;
+            System.out.println("输入第" + count + "个数据，总和是" + sum);
         }
 
         scanner.close();
