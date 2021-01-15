bcf6df136f5b23cf977d34b90c66d4e9ff1a455f
memory125 <mengde1120@sina.com>
2021-01-15 12:37:58 +0800

    update Operator.java

--- java-fundamental-1/src/Operator.java
+++ java-fundamental-1/src/Operator.java
@@ -35,6 +35,27 @@ public class Operator {
         double pow = Math.pow(2, 4);   //16.0
         System.out.println(pow);
 
+        // logic operators
+        boolean x = false;
+        boolean y = true;
+        System.out.println(x && y);         // false - 如有一个为false，结果则为false，两者为true，结果才为true
+        System.out.println(x || y);         // true  - 如有一个为true，结果则为true
+        System.out.println(!(x && y));      // true  - 取反，括号内为true，结果则为false，括号内为false，结果则为true
+
         System.out.println("Operator Demo");
-    }
+
+        // bit operations
+        int x1 = 4;  // 0100
+        int x2 = 5;  // 0101
+        System.out.println(x1 & x2);   // 0100  -  4
+        System.out.println(x1 | x2);   // 0101  -  5
+        System.out.println(x1 ^ x2);   // 0001  -  1
+        System.out.println(~x1);       //  -5
+        System.out.println(~x2);       //  -6
+
+        // shift operators
+        int x3 = 4;
+        System.out.println(x3<<4);              // 64 = x3*2*2*2*2
+        System.out.println(x3>>2);              // 1 = (x3/2)/2
+     }
 }
