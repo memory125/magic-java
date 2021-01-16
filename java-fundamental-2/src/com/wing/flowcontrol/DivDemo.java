1e559aa041dc1d3f9386cc9038ccd1e375f41c2e
memory125 <mengde1120@sina.com>
2021-01-16 15:43:20 +0800

    add OddEvenDemo and DivDemo

---
+++ java-fundamental-2/src/com/wing/flowcontrol/DivDemo.java
@@ -0,0 +1,27 @@
+package com.wing.flowcontrol;
+
+import java.util.Scanner;
+
+public class DivDemo {
+    public static void main(String[] args) {
+        int i = 0;
+        System.out.println("请输入要计算的数字： ");
+        Scanner scanner = new Scanner(System.in);
+        long number = scanner.nextLong();
+
+        while (i <= number) {
+            if (i % 5 == 0) {    // 如果可以被5整除
+                System.out.print(i + "\t\t");
+            }
+
+            // 每行输出3个数字
+            if (i % (5 * 3) == 0) {
+                System.out.println();
+            }
+            i++;
+        }
+
+        scanner.close();
+        System.out.println("------------DivDemo End---------------");
+    }
+}
---
+++ java-fundamental-2/src/com/wing/flowcontrol/OddEvenDemo.java
@@ -0,0 +1,27 @@
+package com.wing.flowcontrol;
+
+import java.util.Scanner;
+
+public class OddEvenDemo {
+    public static void main(String[] args) {
+        int i = 0;
+        long oddSum = 0;    // 奇数和
+        long evenSum = 0;   // 偶数和
+        System.out.println("请输入要计算的数字： ");
+        Scanner scanner = new Scanner(System.in);
+        long number = scanner.nextLong();
+
+        while (i <= number) {
+            if (i % 2 == 0) {    // 偶数
+                evenSum += i;
+            } else {             // 奇数
+                oddSum += i;
+            }
+            i++;
+        }
+
+        scanner.close();
+        System.out.println(number + "以内的奇数和是： " + oddSum);
+        System.out.println(number + "以内的偶数和是： " + evenSum);
+    }
+}
