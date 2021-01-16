dbad0e97a49fe320a294f625d9450e336ac536ae
memory125 <mengde1120@sina.com>
2021-01-16 16:38:50 +0800

    add MultiplyDemo

---
+++ java-fundamental-2/src/com/wing/flowcontrol/MultiplyDemo.java
@@ -0,0 +1,33 @@
+package com.wing.flowcontrol;
+
+import java.util.Scanner;
+
+/**
+ * 1 * 1 = 1
+ * 2 * 1 = 2		2 * 2 = 4
+ * 3 * 1 = 3		3 * 2 = 6		3 * 3 = 9
+ * 4 * 1 = 4		4 * 2 = 8		4 * 3 = 12		4 * 4 = 16
+ * 5 * 1 = 5		5 * 2 = 10		5 * 3 = 15		5 * 4 = 20		5 * 5 = 25
+ * 6 * 1 = 6		6 * 2 = 12		6 * 3 = 18		6 * 4 = 24		6 * 5 = 30		6 * 6 = 36
+ * 7 * 1 = 7		7 * 2 = 14		7 * 3 = 21		7 * 4 = 28		7 * 5 = 35		7 * 6 = 42		7 * 7 = 49
+ * 8 * 1 = 8		8 * 2 = 16		8 * 3 = 24		8 * 4 = 32		8 * 5 = 40		8 * 6 = 48		8 * 7 = 56		8 * 8 = 64
+ * 9 * 1 = 9		9 * 2 = 18		9 * 3 = 27		9 * 4 = 36		9 * 5 = 45		9 * 6 = 54		9 * 7 = 63		9 * 8 = 72		9 * 9 = 81
+ */
+
+public class MultiplyDemo {
+    public static void main(String[] args) {
+        // 打印乘法表
+        System.out.println("请输入要计算的数字： ");
+        Scanner scanner = new Scanner(System.in);
+        long number = scanner.nextLong();
+
+        for (long i = 1; i <= number; i++) {
+            for (long j = 1; j <= i; j++) {
+                System.out.print(i + " * " + j + " = " + (i * j) + "\t\t");
+            }
+            System.out.println();
+        }
+        System.out.println("-----------MultiplyDemo  End-----------------");
+        scanner.close();
+    }
+}
