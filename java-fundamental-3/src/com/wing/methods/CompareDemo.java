eca863e26f5fce53257f06ec730267b8b511d4d2
memory125 <mengde1120@sina.com>
2021-01-18 10:03:12 +0800

    update CompareDemo

--- java-fundamental-3/src/com/wing/methods/CompareDemo.java
+++ java-fundamental-3/src/com/wing/methods/CompareDemo.java
@@ -4,11 +4,19 @@ import java.util.Scanner;
 
 public class CompareDemo {
     private static long max(long a, long b) {
-        return a - b > 0 ? a : b;
+        if (a == b) {
+            return 0;
+        } else {
+            return a - b > 0 ? a : b;
+        }
     }
 
     private static long min(long a, long b) {
-        return a - b < 0 ? a : b;
+        if (a == b) {
+            return 0;
+        } else {
+            return a - b < 0 ? a : b;
+        }
     }
 
     public static void main(String[] args) {
@@ -18,8 +26,18 @@ public class CompareDemo {
         long a = scanner.nextLong();
         long b = scanner.nextLong();
 
-        System.out.println(a + "和" + b + "中" + max(a, b) + "大");
-        System.out.println(a + "和" + b + "中" + min(a, b) + "小");
+        long max = max(a, b);
+        if (max != 0) {
+            System.out.println(a + "和" + b + "中" + max + "大");
+        } else {
+            System.out.println(a + "和" + b + "相等！");
+        }
+        long min = min(a, b);
+        if (min != 0) {
+            System.out.println(a + "和" + b + "中" + min + "小");
+        } else {
+            System.out.println(a + "和" + b + "相等！");
+        }
         scanner.close();
 
         System.out.println("----------- CompareDemo End--------------");
