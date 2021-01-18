f7f25be9fa723e5f267e5e53d011610ec7b5808a
memory125 <mengde1120@sina.com>
2021-01-18 14:54:03 +0800

    add ArrayDemo

---
+++ java-fundamental-3/src/com/wing/methods/ArrayDemo.java
@@ -0,0 +1,53 @@
+package com.wing.methods;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ArrayDemo {
+    private static void printArray(long[] array) {
+        for (long arr: array) {
+            System.out.print(arr + "  ");
+        }
+        System.out.println();
+    }
+
+    private static long[] reverseArray(long[] array) {
+        assert array.length != 0;
+
+        long[] result = new long[array.length];
+
+        for (int i = 0, j = array.length - 1; i < array.length; i++, j--) {
+            result[j] = array[i];
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入要比较的数据： ");
+
+        List<Long> longArr = new ArrayList<>();
+
+        long input = 0;
+        while (scanner.hasNextLong()) {
+            input = scanner.nextLong();
+            longArr.add(input);
+        }
+
+        // 将List<Long>转成long[]
+        long [] array = longArr.stream().mapToLong(t->t.longValue()).toArray();
+
+        System.out.println("您输入的数组是: ");
+        // 打印数组
+        printArray(array);
+
+        // 反转数组
+        long[] reversedArray = reverseArray(array);
+        System.out.println("反转后的数组是: ");
+        printArray(reversedArray);
+
+        scanner.close();
+    }
+}
