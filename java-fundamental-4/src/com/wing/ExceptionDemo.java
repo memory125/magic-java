8ea76bb88614a3c7ffbc54f0bae8e86592541859
memory125 <mengde1120@sina.com>
2021-01-20 18:57:57 +0800

    update ExceptionDemo

--- java-fundamental-4/src/com/wing/ExceptionDemo.java
+++ java-fundamental-4/src/com/wing/ExceptionDemo.java
@@ -8,11 +8,11 @@ public class ExceptionDemo {
         int a = 8;
         int b = 0;
 
-        try {
+        try {                                     // try监控区域
             System.out.println(a / b);
-        } catch (ArithmeticException e) {
+        } catch (ArithmeticException e) {         // catch 捕获异常
             System.out.println(e.toString());
-        } finally {
+        } finally {                               // 处理善后工作
             System.out.println("Please check your parameters.");
         }
     }
