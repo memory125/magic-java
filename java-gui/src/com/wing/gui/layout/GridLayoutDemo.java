cf358aefda9de6db3560f0da426ff007b5680df9
memory125 <mengde1120@sina.com>
2021-01-25 21:47:12 +0800

    add GridLayoutDemo

---
+++ java-gui/src/com/wing/gui/layout/GridLayoutDemo.java
@@ -0,0 +1,53 @@
+package com.wing.gui.layout;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class GridLayoutDemo {
+    public static void main(String[] args) {
+        // 创建Frame对象
+        Frame frame = new Frame("GridLayoutDemo");
+
+        // 设置大小
+        frame.setSize(400, 400);
+        // 设置背景颜色
+        frame.setBackground(new Color(129, 175, 201));
+        // 设置起始位置
+        frame.setLocation(200, 200);
+
+        // 设置布局
+        frame.setLayout(new GridLayout(2, 3, 30,20));
+
+        // 按钮
+        Button button1 = new Button("Button1");
+        Button button2 = new Button("Button2");
+        Button button3 = new Button("Button3");
+        Button button4 = new Button("Button4");
+        Button button5 = new Button("Button5");
+        Button button6 = new Button("Button6");
+
+
+        // 添加按钮
+        frame.add(button1);
+        frame.add(button2);
+        frame.add(button3);
+        frame.add(button4);
+        frame.add(button5);
+        frame.add(button6);
+
+
+        // 设置可见
+        frame.setVisible(true);
+
+        // 添加监听事件
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+                System.exit(0);
+            }
+        });
+
+    }
+}
