ab7be3a7a1c0f149f8f35c1f5aa906ccc0870a34
memory125 <mengde1120@sina.com>
2021-01-26 15:56:01 +0800

    add TextFieldDemo

---
+++ java-gui/src/com/wing/gui/textfield/TextFieldDemo.java
@@ -0,0 +1,56 @@
+package com.wing.gui.textfield;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class TextFieldDemo {
+    public static void main(String[] args) {
+        new TextFieldFrame();
+    }
+}
+
+class TextFieldFrame extends Frame {
+    public TextFieldFrame() {
+        loadFrame();
+    }
+
+    public static void closeFrame(Frame frame) {
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    public void loadFrame() {
+        // textField
+        TextField textField = new TextField(20);
+
+        textField.addActionListener(new MyTextFieldFrameActionListener());
+        add(textField);
+
+        setLocation(200,200);
+        setBackground(Color.ORANGE);
+        pack();
+
+        setVisible(true);
+        closeFrame(this);
+    }
+}
+
+class MyTextFieldFrameActionListener implements ActionListener {
+
+    public MyTextFieldFrameActionListener() {
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        TextField textField = (TextField) e.getSource();
+        System.out.println(textField.getText());
+    }
+}
+
