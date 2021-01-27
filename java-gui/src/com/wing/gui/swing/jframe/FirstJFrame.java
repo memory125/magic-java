0d43ec8fbf5d2c636081125f7b9e9365d1f5a2ea
memory125 <mengde1120@sina.com>
2021-01-27 11:11:39 +0800

    update jframe structure

--- java-gui/src/com/wing/gui/swing/jframe/FirstJFrame.java
+++ java-gui/src/com/wing/gui/swing/jframe/FirstJFrame.java
@@ -5,19 +5,19 @@ import java.awt.*;
 /**
  * @author memory125
  */
-public class FirstJFrame {
+public class FirstJFrame extends JFrame {
     public void init() {
-        // 创建窗口
-        JFrame jFrame = new JFrame("FirstJFrame");
+        // 设置窗口标题
+        setTitle("FirstJFrame");
         // 设置窗口属性
-        jFrame.setBounds(200,200,400,400);
-        jFrame.setVisible(true);
+        setBounds(200,200,400,400);
+        setVisible(true);
 
         // 设置背景颜色
-        Container contentPane = jFrame.getContentPane();
+        Container contentPane = getContentPane();
         contentPane.setBackground(new Color(31, 227, 117));
 
         // 设置默认关闭事件
-        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 }
