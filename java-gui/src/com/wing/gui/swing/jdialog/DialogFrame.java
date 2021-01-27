33b0f9c14d6ac551e8244090629feddeb3ecc2ec
memory125 <mengde1120@sina.com>
2021-01-27 12:16:29 +0800

    add DialogFrame

---
+++ java-gui/src/com/wing/gui/swing/jdialog/DialogFrame.java
@@ -0,0 +1,40 @@
+package com.wing.gui.swing.jdialog;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * @author memory125
+ */
+public class DialogFrame extends JFrame{
+
+    //JDialog jDialog;
+
+    public void init() {
+        setBounds(200,200,400,400);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+
+        Container container = getContentPane();
+        container.setBackground(new Color(118, 229, 182));
+        container.setLayout(null);
+
+        // 按钮
+        JButton popupButton = new JButton("Popup");
+        popupButton.setBounds(30,30,80,60);
+        popupButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JDialog jDialog = new JDialog();
+                jDialog.setTitle("Popup Dialog");
+                jDialog.setVisible(true);
+                jDialog.setBounds(200,200, 200,200);
+                jDialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+                //jDialog.dispose();
+            }
+        });
+        container.add(popupButton);
+    }
+}
---
+++ java-gui/src/com/wing/gui/swing/jdialog/DialogFrameDemo.java
@@ -0,0 +1,10 @@
+package com.wing.gui.swing.jdialog;
+
+/**
+ * @author memory125
+ */
+public class DialogFrameDemo {
+    public static void main(String[] args) {
+        new DialogFrame().init();
+    }
+}
