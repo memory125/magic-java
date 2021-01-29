30d1416dabe0437dda95c54f97ba8bc12f2e830f
memory125 <mengde1120@sina.com>
2021-01-29 20:34:13 +0800

    update TestThread1

--- java-thread/src/com/wing/thread/TestThread1.java
+++ java-thread/src/com/wing/thread/TestThread1.java
@@ -4,6 +4,7 @@ package com.wing.thread;
  * @author memory125
  */
 // 创建线程方式一: 继承Thread类，重写run()方法，条用start()开启线程
+// 总结：注意，线程开启不一定要立即执行，由CPU调度执行
 public class TestThread1 extends Thread{
     public static void main(String[] args) {
         // 创建一个线程对象
