1e046709da51decd8044d865754d3a319c2ccb8b
memory125 <mengde1120@sina.com>
2021-01-30 15:59:22 +0800

    add TestThread3

---
+++ java-thread/src/com/wing/runnable/TestThread3.java
@@ -0,0 +1,33 @@
+package com.wing.runnable;
+
+import com.wing.thread.TestThread1;
+
+// 创建线程方式2：实现Runnable接口，重写run方法，执行线程需要丢入Runnable接口的实现类，调用start方法
+public class TestThread3 implements Runnable {
+    public static void main(String[] args) {
+        // 1. 创建runnable接口的实现类对象
+        //TestThread3 testThread3 = new TestThread3();
+
+        // 2. 创建线程对象，通过线程对象来开启线程，代理
+        //Thread thread = new Thread(testThread3);
+
+        // 3. 开启线程
+        //thread.start();
+
+        // 启动线程
+        new Thread(new TestThread3()).start();
+
+        // main线程
+        for (int i = 0; i < 200; i++) {
+            System.out.println("I'm coding, this is main thread!!!" + i);
+        }
+    }
+
+    @Override
+    public void run() {
+        // run方法线程体
+        for (int i = 0; i < 200; i++) {
+            System.out.println("I'm learning Java-thread Runnable programming!!!" + i);
+        }
+    }
+}
