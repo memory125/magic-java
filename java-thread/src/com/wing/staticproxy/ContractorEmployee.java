c99d43c2ffb0256b23bee62b0ce14f4a2ddf0fa1
memory125 <mengde1120@sina.com>
2021-02-01 11:27:56 +0800

    add staticproxy

--- java-thread/src/com/wing/staticproxy/ContractorEmployee.java
+++ java-thread/src/com/wing/staticproxy/ContractorEmployee.java
@@ -5,6 +5,6 @@ package com.wing.staticproxy;
  */
 // 雇佣者，服务于具体的雇佣公司
 // 职责: 参与具体的工作
-public interface ContractorEmplyee {
+public interface ContractorEmployee {
     void mission(String mission);
 }
--- java-thread/src/com/wing/staticproxy/ContractorVendor.java
+++ java-thread/src/com/wing/staticproxy/ContractorVendor.java
@@ -4,7 +4,7 @@ package com.wing.staticproxy;
  * @author memory125
  */
 // 雇佣公司
-public class ContractorVendor implements ContractorEmplyee {
+public class ContractorVendor implements ContractorEmployee {
 
     // 具体的雇佣者
     private Employee employee;
