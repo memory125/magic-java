3a5bd0f25937dddcc7f3492d24d30b2803879582
memory125 <mengde1120@sina.com>
2021-02-05 19:35:02 +0800

    update AnnonationTest

--- java-annotation-reflector/src/com/wing/buildin/AnnonationTest.java
+++ java-annotation-reflector/src/com/wing/buildin/AnnonationTest.java
@@ -6,24 +6,18 @@ import java.util.List;
 /**
  * @author memory125
  */
-// 什么是注解
+// 什么是注释
+// @SuppressWarnings("all")
 public class AnnonationTest {
     // 内置注解2
     // @Deprecated：废弃的注解，不推荐程序员使用，但可以使用，或者存在更好的方式
     @Deprecated
     public static void test1() {
-        System.out.println("test=========>");
+        System.out.println("test=========>@Deprecated");
     }
 
     public static void main(String[] args) {
         test1();
-    }
-
-    // 内置注解1
-    // @Override: 重写的注解
-    @Override
-    public String toString() {
-        return super.toString();
     }
 
     // 内置注解3
@@ -34,4 +28,10 @@ public class AnnonationTest {
         List list = new ArrayList();
     }
 
+    // 内置注解1
+    // @Override: 重写的注解
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
