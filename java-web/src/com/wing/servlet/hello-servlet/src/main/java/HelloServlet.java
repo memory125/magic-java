5c2b4e7b37ef0e670315416a4abbf2950997697a
memory125 <mengde1120@sina.com>
2021-02-20 16:40:01 +0800

    create webapp

---
+++ java-web/src/com/wing/servlet/hello-servlet/src/main/java/HelloServlet.java
@@ -0,0 +1,32 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author memory125
+ */
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("utf-8");
+        PrintWriter writer = resp.getWriter();
+
+        writer.println("<html>");
+        writer.println("<head>");
+        writer.println("<title>你好！Servlet！</title>");
+        writer.println("</head>");
+        writer.println("<body>");
+        writer.println("<h1>第一个Servlet程序</h1>");
+        writer.println("<body>");
+        writer.println("</html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       doGet(req, resp);
+    }
+}
