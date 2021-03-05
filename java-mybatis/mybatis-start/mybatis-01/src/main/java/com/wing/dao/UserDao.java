d1a478bed8bc9455f55aa4e706daad39757e86f9
memory125 <mengde1120@sina.com>
2021-03-05 17:09:37 +0800

    update mybatis project

--- java-mybatis/mybatis-start/mybatis-01/src/main/java/com/wing/dao/UserDao.java
+++ java-mybatis/mybatis-start/mybatis-01/src/main/java/com/wing/dao/UserDao.java
@@ -9,4 +9,5 @@ import java.util.List;
  */
 public interface UserDao {
     List<User> getUserList();
+    User getUserById(int id);
 }
--- java-mybatis/mybatis-start/mybatis-01/src/test/java/com/wing/dao/UserTest.java
+++ java-mybatis/mybatis-start/mybatis-01/src/test/java/com/wing/dao/UserTest.java
@@ -25,4 +25,17 @@ public class UserTest {
         // 关闭资源
         sqlSession.close();
     }
+
+    @Test
+    public void getUserById() {
+        // 获取sqlSession对象
+        SqlSession sqlSession = MyBatisUtils.getSqlSession();
+        // 执行
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+        User user = userDao.getUserById(5);
+        System.out.println(user);
+
+        // 关闭资源
+        sqlSession.close();
+    }
 }
