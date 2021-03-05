a32a8fe9b955a7f4c51a143be8811ee0589138f6
memory125 <mengde1120@sina.com>
2021-03-05 16:25:02 +0800

    update mybatis project

--- java-mybatis/mybatis-start/mybatis-01/src/main/java/com/wing/dao/UserDao.java
+++ java-mybatis/mybatis-start/mybatis-01/src/main/java/com/wing/dao/UserDao.java
@@ -1,4 +1,12 @@
 package com.wing.dao;
 
+import com.wing.pojo.User;
+
+import java.util.List;
+
+/**
+ * @author memory125
+ */
 public interface UserDao {
+    List<User> getUserList();
 }
---
+++ java-mybatis/mybatis-start/mybatis-01/src/test/java/com/wing/dao/UserTest.java
@@ -0,0 +1,28 @@
+package com.wing.dao;
+
+import com.wing.pojo.User;
+import com.wing.utils.MyBatisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * @author memory125
+ */
+public class UserTest {
+    @Test
+    public void getUsers() {
+        // 获取sqlSession对象
+        SqlSession sqlSession = MyBatisUtils.getSqlSession();
+        // 执行
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+        List<User> userList = userDao.getUserList();
+        for (User user : userList) {
+            System.out.println(user);
+        }
+
+        // 关闭资源
+        sqlSession.close();
+    }
+}
