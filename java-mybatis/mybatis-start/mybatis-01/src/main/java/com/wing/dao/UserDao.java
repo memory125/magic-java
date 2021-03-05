dc01cb4287a8666b7650ff0387d6029de067bfaa
memory125 <mengde1120@sina.com>
2021-03-05 17:27:53 +0800

    add user operation

--- java-mybatis/mybatis-start/mybatis-01/src/main/java/com/wing/dao/UserDao.java
+++ java-mybatis/mybatis-start/mybatis-01/src/main/java/com/wing/dao/UserDao.java
@@ -8,6 +8,18 @@ import java.util.List;
  * @author memory125
  */
 public interface UserDao {
+    // 获取所有用户
     List<User> getUserList();
+
+    // 根据用户ID查询
     User getUserById(int id);
+
+    // 添加用户
+    int addUser(User user);
+
+    // 修改用户
+    int updateUser(User user);
+
+    // 删除用户
+    int deleteUser(int id);
 }
--- java-mybatis/mybatis-start/mybatis-01/src/test/java/com/wing/dao/UserTest.java
+++ java-mybatis/mybatis-start/mybatis-01/src/test/java/com/wing/dao/UserTest.java
@@ -38,4 +38,62 @@ public class UserTest {
         // 关闭资源
         sqlSession.close();
     }
+
+    // 增删改需要提交事务
+    @Test
+    public void addUser() {
+        // 获取sqlSession对象
+        SqlSession sqlSession = MyBatisUtils.getSqlSession();
+
+        // 执行
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+        int result = userDao.addUser(new User(8, "Clark", "555666"));
+        if (result > 0) {
+            System.out.println("添加用户成功！");
+        }
+
+        // 必须要提交事务
+        sqlSession.commit();
+
+        // 关闭资源
+        sqlSession.close();
+    }
+
+    // 增删改需要提交事务
+    @Test
+    public void updateUser() {
+        // 获取sqlSession对象
+        SqlSession sqlSession = MyBatisUtils.getSqlSession();
+
+        // 执行
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+        int result = userDao.updateUser(new User(8, "Celina", "222333"));
+        if (result > 0) {
+            System.out.println("修改用户成功！");
+        }
+
+        // 必须要提交事务
+        sqlSession.commit();
+
+        // 关闭资源
+        sqlSession.close();
+    }
+
+    @Test
+    public void deleteUser() {
+        // 获取sqlSession对象
+        SqlSession sqlSession = MyBatisUtils.getSqlSession();
+
+        // 执行
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+        int result = userDao.deleteUser(8);
+        if (result > 0) {
+            System.out.println("删除用户成功！");
+        }
+        // 必须要提交事务
+        sqlSession.commit();
+
+        // 关闭资源
+        sqlSession.close();
+    }
 }
